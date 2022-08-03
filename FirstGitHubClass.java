00c442637cb22cbb3748c5ee8d78147d547c7a37
Tong0316 <104473397+Tong0316@users.noreply.github.com>
2022-08-03 18:13:11 +0800

    Add files via upload

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -2,6 +2,6 @@ package cn.bdqn.kgc;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello,GitHub!");
+        System.out.println("My GitHub class!");
     }
 }
